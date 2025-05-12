01e452ab68663fa06ac27501acc2df5087cef8ba
Your name <Your email>
2025-05-12 11:55:47 +0200

    readme

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-const app = require('../app');
+router.get('/', (req, res) => {
+  res.send('Weather API is running.');
+});
 
 module.exports = router;
-
